7e761a53e34b43dff5646f1c4788b8c230c29b5d
asiapicasso <asia.picasso@gmail.com>
2024-06-23 11:54:28 +0200

    update banner

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,17 +1,15 @@
-import { Github } from "@medusajs/icons"
+import { Link } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+    <div className="h-[30vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <div className="absolute inset-0 z-6 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Site fermé pour le week-end. Venez en boutique !
-          </Heading>
+          > Banner's title </Heading>
         </span>
         <a
           href="https://maps.app.goo.gl/Kd4JEZ164rgA1YyV8"
@@ -19,7 +17,7 @@ const Hero = () => {
         >
           <Button variant="secondary">
             Notre showroom
-            <Github />
+            <Link />
           </Button>
         </a>
       </div>
